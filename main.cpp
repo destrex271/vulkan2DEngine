369887d822b6ad0e6390acb0b60e68de48a17eb2
destrex271 <destrex271@gmail.com>
2022-12-28 20:47:37 +0530

    Open GLFWwindow setup done

--- main.cpp
+++ main.cpp
@@ -1,35 +1,56 @@
+#include <exception>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+class BaseShapesVulkan{
+	public:
+		void run(){
+			initWindow();
+			initVulkan();
+			mainLoop();
+			cleanUp();
+		}
+	
+	private:
+		
+		GLFWwindow* window;
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+		// specifying window size parameters
+		const uint32_t WIDTH = 800;
+		const uint32_t HEIGHT = 600;
+		const char* TITLE = "VULKAN WINDOW";
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		void initWindow(){
+			glfwInit(); // Initialize window of Graphics Library Framework
+			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Disable the call to default opengl api byglfw to replace it by vulkan later
+			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Diable Resize ability of window
 
-    std::cout << extensionCount << " extensions supported\n";
+			window = glfwCreateWindow(WIDTH, HEIGHT, TITLE,nullptr, nullptr); // monitor stands for which monitor to display window on
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+		}
+		void initVulkan(){}
+		void mainLoop(){
+			while(!glfwWindowShouldClose(window)){
+				glfwPollEvents();
+			}
+		}
+		void cleanUp(){
+			glfwDestroyWindow(window);
+			glfwTerminate();
+		}
+};
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+int main(){
+	BaseShapesVulkan app;
+	try{
+		std::cout << "opening window" << std::endl;
+		app.run();
+	} catch(const std::exception& e){
+		std::cerr << e.what() << std::endl;
+		return  EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
